d02ab19a4d957c30b1e4a139dad644f01a53e5b3
sdibyanshu <sdibyanshu>
2022-09-28 23:04:40 +0530

    Day-2 updated

---
+++ outlook_india/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbarg from './Components/Navbar';
+import Newsg from './Components/News';
+
+function App() {
+  return (
+    <div className="App">
+        <Navbarg/>
+        <Newsg/>
+        <Newsg/>
+    </div>
+  );
+}
+
+export default App;
